18bf6f16800dfae030dc149b21992ebd4e64c08e
Skaruts <Skaruts@users.noreply.github.com>
2025-07-12 03:15:32 +0100

    clean up

--- addons/sk_fly_camera/plugin.gd
+++ addons/sk_fly_camera/plugin.gd
@@ -2,12 +2,4 @@
 extends EditorPlugin
 
 
-# const _shared := preload("res://addons/sk_racing_cameras/src/shared.gd")
-
-
-# func _enter_tree() -> void:
-# 	add_autoload_singleton(_shared.SINGLETON_NAME, "res://addons/sk_racing_cameras/src/cameraman.gd")
-
-# func _exit_tree() -> void:
-# 	remove_autoload_singleton(_shared.SINGLETON_NAME)
-
+# nothing to see here
